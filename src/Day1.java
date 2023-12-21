1fdea64a3db745b25979022315b14853736b00fe
Bachagha Mousaab <Mossbachagha@gmail.com>
2023-12-21 19:39:57 +0100

    hashmap to arraylist

--- src/Day1.java
+++ src/Day1.java
@@ -9,46 +9,38 @@ import java.util.Map;
 public class Day1 {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\MSI\\IdeaProjects\\firstProg\\AdventOfCode2023\\src\\input.txt"));
-        Map<Integer, String> numbers = new HashMap<>();
-        numbers.put(1, "one");
-        numbers.put(2, "two");
-        numbers.put(3, "three");
-        numbers.put(4, "four");
-        numbers.put(5, "five");
-        numbers.put(6, "six");
-        numbers.put(7, "seven");
-        numbers.put(8, "eight");
-        numbers.put(9, "nine");
-        String line = new String();
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\MSI\\IdeaProjects\\firstProg\\AdventOfCode\\src\\inputs.txt"));
+        String line;
+        List<String> numbers = new ArrayList<>();
+        numbers.add("one");
+        numbers.add("two");
+        numbers.add("three");
+        numbers.add("four");
+        numbers.add("five");
+        numbers.add("six");
+        numbers.add("seven");
+        numbers.add("eight");
+        numbers.add("nine");
         int result = 0;
-        int index = 0;
-        while ((line = reader.readLine()) != null){
+        while((line = reader.readLine()) != null){
+            String token = "";
             List<Integer> integers = new ArrayList<>();
-
-            String token = new String();
             for (int i = 0; i < line.length(); i++) {
                 if (Character.isDigit(line.charAt(i))){
                     integers.add(Character.getNumericValue(line.charAt(i)));
                     token = "";
                 }else{
                     token = token + line.charAt(i);
-                    for (int j = 1; j <= numbers.size(); j++) {
+                    for (int j = 0; j < numbers.size(); j++) {
                         if (token.contains(numbers.get(j))){
-                            integers.add(j);
-                            token = String.valueOf(token.charAt(token.length() -1));
+                            integers.add(j+1);
+                            token = String.valueOf(token.charAt(token.length()-1));
                         }
                     }
                 }
-
             }
-
             result = result + integers.getFirst()*10 + integers.getLast();
-            System.out.println(integers.getFirst()*10 + integers.getLast());
-
         }
-
         System.out.println(result);
-
     }
 }
